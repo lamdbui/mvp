c8a0809dd23a6a3709e7fcf30914d3d18ed4b4a5
Lam Bui <lamdbui@gmail.com>
2017-09-25 15:23:22 -0700

    Add some initial boilerplate for petfinder api interface

--- server/index.js
+++ server/index.js
@@ -1,11 +1,25 @@
 const express = require('express');
 const database = require('../database');
+const petfinder = require('../data/petfinder');
 const app = express();
 
 const PORT = process.env.PORT || 3333;
 
 app.get('/', (request, response) => {
   response.send('Yay a response!');
+});
+
+app.get('/pets', (request, response) => {
+  console.log('ATTEMPTING TO GET /pets');
+  petfinder.getPets()
+    .then(resolve => {
+      console.log('GET RESOLVE /pets:', resolve.length);
+      response.status(200).send(resolve);
+    })
+    .catch(reject => {
+      console.log('GET REJECT /pets:', reject);
+      response.status(400).send(reject);
+    });
 });
 
 app.listen(PORT, () => {
