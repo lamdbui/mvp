b54d63866b6b34b57f458050789e4c5dbbe8c5b5
Lam Bui <lamdbui@gmail.com>
2017-09-25 12:00:18 -0700

    Add working basic client template

---
+++ client/src/components/App.jsx
@@ -0,0 +1,12 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+//
+// // ReactDOM.render(
+// //   <h1>MyApp</h1>
+// // );
+
+var App = (props) => (
+  <h2>My App</h2>
+);
+
+window.App = App;
