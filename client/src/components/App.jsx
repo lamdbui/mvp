7805e788eb2c34d86346614782451ccf9e28c670
Lam Bui <lamdbui@gmail.com>
2017-09-26 11:27:02 -0700

    Add client-side filtering by breed

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -2,7 +2,11 @@ class App extends React.Component {
   constructor(props) {
     super(props);
 
+    this.ALL_BREEDS = 'ALL BREEDS';
+
     this.state = {
+      breeds: [],
+      currentSelectedBreed: 'ALL BREEDS',
       pets: [],
       currentPets: [],
       filterByFavorites: false,
@@ -12,6 +16,7 @@ class App extends React.Component {
     this.handleShowFavoritesClick = this.handleShowFavoritesClick.bind(this);
     this.handleShowAllClick = this.handleShowAllClick.bind(this);
     this.setLocationStateHandler = this.setLocationStateHandler.bind(this);
+    this.handleBreedSelect = this.handleBreedSelect.bind(this);
   }
 
   setLocationStateHandler(location) {
@@ -48,11 +53,34 @@ class App extends React.Component {
     });
   }
 
+  handleBreedSelect(event) {
+    let selectedBreed = event.target.value;
+    console.log('*** SELECTED BREED:', selectedBreed);
+    // reset to show all pets when ALL_BREEDS is selected
+    if (selectedBreed === this.ALL_BREEDS) {
+      this.setState({
+        currentSelectedBreed: this.ALL_BREEDS,
+        currentPets: this.state.pets
+      });
+    } else {
+      // do filtering here
+      let filteredPets = this.state.pets.filter(pet => {
+        return pet.breed.includes(selectedBreed);
+      });
+      // console.log('*** FILTERED PETS:', filteredPets);
+      this.setState({
+        currentSelectedBreed: event.target.value,
+        currentPets: filteredPets
+      });
+    }
+  }
+
   handleShowAllClick() {
     this.setState({ filterByFavorites: true, currentPets: this.state.pets });
   }
 
   componentDidMount() {
+    // TODO: maybe put these in separate util functions
     $.ajax({
       method: 'GET',
       url: '/pets',
@@ -63,6 +91,19 @@ class App extends React.Component {
         console.log('*** THE SADNESS -', data);
       }
     });
+
+    $.ajax({
+      method: 'GET',
+      url: '/breeds',
+      success: (data) => {
+        // console.log('*** RETRIEVED BREEDS');
+        this.setState({ breeds: data });
+      },
+      error: (data) => {
+        console.log('*** THE SADNESS -', data);
+        // this.setState({ filterByFavorites: true });
+      }
+    });
   }
 
   render() {
@@ -71,6 +112,13 @@ class App extends React.Component {
         <h2>DogAdopt.us</h2>
         <button type="button" onClick={this.handleShowFavoritesClick}>Show Favorites</button>
         <button type="button" onClick={this.handleShowAllClick}>Show All</button>
+        <p>
+          <label>Select Breed: </label>
+          <select id="breed_list" onChange={this.handleBreedSelect}>
+            <option value={this.ALL_BREEDS}>{this.ALL_BREEDS}</option>
+            {this.state.breeds.map((breed, key) => <option key={key} value={breed}>{breed}</option>)}
+          </select>
+        </p>
         <Search currentLocation={this.state.currentLocation} setLocationCallback={this.setLocationStateHandler}></Search>
         <PetList pets={this.state.currentPets}></PetList>
       </div>
