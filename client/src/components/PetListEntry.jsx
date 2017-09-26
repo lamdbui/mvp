78e116558dcfb7dcd747b2dcdc6e7cf710f05a59
Lam Bui <lamdbui@gmail.com>
2017-09-25 20:10:51 -0700

    Add a favorite button to PetListEntry

--- client/src/components/PetListEntry.jsx
+++ client/src/components/PetListEntry.jsx
@@ -1,7 +1,30 @@
-var PetListEntry = (props) => (
-  <div>
-    <h4>{props.pet.name}</h4>
-    <img src={props.pet.photoUrls[0]}></img>
-    <p>{props.pet.description}</p>
-  </div>
-);
+class PetListEntry extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.handleFavoriteClick = this.handleFavoriteClick.bind(this);
+  }
+
+  handleFavoriteClick() {
+    console.log('CLICKED FAVORITE: ', this.props.pet.petId);
+  }
+
+  render() {
+    return ( <div>
+        <h4>{this.props.pet.name}</h4>
+        <img src={this.props.pet.photoUrls[0]}></img>
+        <button type="button" onClick={this.handleFavoriteClick}>Add to favorites</button>
+        <p>{this.props.pet.description}</p>
+      </div>
+    );
+  }
+}
+
+// var PetListEntry = (props) => (
+//   <div>
+//     <h4>{props.pet.name}</h4>
+//     <img src={props.pet.photoUrls[0]}></img>
+//     <button type="button">Add to favorites</button>
+//     <p>{props.pet.description}</p>
+//   </div>
+// );
