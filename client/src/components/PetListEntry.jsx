41180992040613536a65676ce4a0cf2c55c6129a
Lam Bui <lamdbui@gmail.com>
2017-11-15 10:17:54 -0800

    Adjust position for some minor UI elements

--- client/src/components/PetListEntry.jsx
+++ client/src/components/PetListEntry.jsx
@@ -25,40 +25,44 @@ class PetListEntry extends React.Component {
       <div className="pet-list-entry">
         <hr></hr>
         <h3>{this.props.pet.name.toUpperCase()}</h3>
-        <div className="pet-list-entry-profile">
-          <img src={this.props.pet.photoUrls[1]}></img>
+        <div>
+          <div className="pet-list-entry-profile">
+            <img src={this.props.pet.photoUrls[1]}></img>
+            {/* <button type="button" onClick={this.handleFavoriteClick}>Add to favorites</button> */}
+          </div>
           {/* <button type="button" onClick={this.handleFavoriteClick}>Add to favorites</button> */}
-        </div>
-        <button type="button" onClick={this.handleFavoriteClick}>Add to favorites</button>
-        <div className="pet-list-entry-details">
-          <div><b>Sex:</b> {(this.props.pet.sex === 'F') ? 'Female' : 'Male'}</div>
-          <div><b>Age:</b> {this.props.pet.ageClass}</div>
+          <div className="pet-list-entry-details">
+            <div><b>Sex:</b> {(this.props.pet.sex === 'F') ? 'Female' : 'Male'}</div>
+            <div><b>Age:</b> {this.props.pet.ageClass}</div>
 
-          {/* <button type="button" onClick={this.handleFavoriteClick}>Add to favorites</button> */}
-          <div>
-            <table>
-              <tbody>
-                <tr>
-                  <th>City</th>
-                  {/* <th>State</th> */}
-                  <th>Phone Number</th>
-                  <th>Email</th>
-                </tr>
-                <tr>
-                  <td>{this.props.pet.contactCity}</td>
-                  {/* <td>{this.props.pet.contactState}</td> */}
-                  <td>{this.props.pet.contactPhoneNumber}</td>
-                  <td>{this.props.pet.contactEmail}</td>
-                </tr>
-              </tbody>
-            </table>
+            {/* <button type="button" onClick={this.handleFavoriteClick}>Add to favorites</button> */}
+            <div>
+              <table>
+                <tbody>
+                  <tr>
+                    <th>City</th>
+                    {/* <th>State</th> */}
+                    <th>Phone Number</th>
+                    <th>Email</th>
+                  </tr>
+                  <tr>
+                    <td>{this.props.pet.contactCity}</td>
+                    {/* <td>{this.props.pet.contactState}</td> */}
+                    <td>{this.props.pet.contactPhoneNumber}</td>
+                    <td>{this.props.pet.contactEmail}</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
           </div>
         {/* </div> */}
         {/* <button type="button" onClick={this.handleFavoriteClick}>Add to favorites</button> */}
         {/* <div> */}
-          {/* <button type="button" onClick={this.handleFavoriteClick}>Add to favorites</button> */}
-          <h4>Description</h4>
-          <p>{this.props.pet.description}</p>
+          <div>
+            <button type="button" onClick={this.handleFavoriteClick}>Add to favorites</button>
+            <h4>Description</h4>
+            <p>{this.props.pet.description}</p>
+          </div>
         </div>
       </div>
     );
